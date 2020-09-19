6ee0f83b06b5abb59ad86e6f30551f8c96420d73
artur <zhitenev.artur@yandex.ru>
2020-09-19 20:26:15 +0300

    almost done

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-function goToNextStage () {
+questionNumb = 1;
+let answersFirst = [];
+let answersSecond = [];
+let timeToTest = 180; 
+
+function showFirstQuestion() {
 	document.getElementById('instructions').style.display = 'none';
 	document.getElementById('testing').style.display = 'flex';
 	countdown();
 }
 
-let start = 180;
+let start = timeToTest;
 let timer;
 
 function countdown () {
 	document.getElementById('clock').innerHTML = secToMin(start);
-	start--; // уменьшаем число на единицу
+	start--;
 	if (start<0){
-		clearTimeout(timer); // таймер остановится на нуле
-    	alert('stop');
+		clearTimeout(timer);
+		questionNumb++;
+		if (questionNumb == 2) {
+			showDummyMessage()
+		}
+		else alert('end');
+    	
 	}
 	else {
 		timer = setTimeout(countdown, 1000);
@@ -28,6 +38,25 @@ function secToMin (seconds) {
 
 function getAnswer () {
 	var inputValue = document.getElementById("userAnswer").value;
+	if (questionNumb == 1){
+		answersFirst.push(inputValue);
+	}
+	else answersSecond.push(inputValue);
 	document.getElementById("userAnswer").value = '';
 	document.getElementById("answList").innerHTML += "<p>"+inputValue+"</p>";
+}
+
+function showDummyMessage () {
+	document.getElementById('testing').style.display = 'none';
+	document.getElementById('dummy').style.display = 'block';
+}
+
+function showSecondQuestion () {
+	document.getElementById('dummy').style.display = 'none';
+	document.getElementById('testing').style.display = 'flex';
+	document.getElementById('img').innerHTML = document.getElementById('img').innerHTML.replace('newspapper.png', 'liner.png');
+	document.getElementById('answList').innerHTML = '<p>Здесь будут появляться ваши ответы:</p>';
+	start = timeToTest;
+	timer = 0;
+	countdown();
 }
